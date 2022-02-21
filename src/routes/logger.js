246e13e91ccef9ea30ff6fbe31de436c388828fc
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-02-21 17:20:20 +0530

    change in logger file and route file

---
+++ src/routes/logger.js
@@ -0,0 +1,7 @@
+let url = "google"
+function myFunc(msg){
+    console.log(msg)
+}
+
+module.exports.url = url;
+module.exports.myFunc = myFunc
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
+let obj = require("./logger")
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
+    console.log(obj.url)
+    obj.myFunc("Thorium")
 });
 
 module.exports = router;
