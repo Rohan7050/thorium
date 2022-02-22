04052b62f2700058e10d4ff8e45e2116030b7d98
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-02-22 09:38:09 +0530

    assignment complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,7 +8,7 @@ const lod = require("../lodashMod/lod")
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
     console.log(obj.url)
-    obj.welcome("TWelcome to my application. I am Rohan Pagare and a part of FunctionUp Thorium cohort.")
+    obj.welcome("Welcome to my application. I am Rohan Pagare and a part of FunctionUp Thorium cohort.")
     helper.printDate()
     helper.printMonth()
     helper.getBatchInfo()
@@ -19,10 +19,10 @@ router.get('/test-me', function (req, res) {
 
 router.get("/hello", function(req, res){
     res.send("hello world")
-    console.log(lod.arrSpliter())
-    console.log(lod.giveTail())
-    console.log(lod.arrUnion())
-    console.log(lod.getPairs())
+    lod.arrSpliter()
+    lod.giveTail()
+    lod.arrUnion()
+    lod.getPairs()
 })
 
 module.exports = router;
