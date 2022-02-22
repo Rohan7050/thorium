09a1975490bdf41170deb2433a9b5e9d94ad8985
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-02-22 19:52:29 +0530

    assignment complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,48 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+const movies = [{
+    id: 1,
+    "name": "The Shining"
+   }, {
+    id: 2,
+    "name": "Incendies"
+   }, {
+    id: 3,
+    "name": "Rang de Basanti"
+   }, {
+    id: 4,
+    "name": "Finding Demo"
+   }]
+let moviesarr = ["avengers", "spiderman", "dogs purpose", "abcd"]
+
+
+router.get("/movies", function(req, res){
+    res.send(moviesarr)
 })
 
+router.get("/movies/:index", function(req, res) {
+    let i = req.params.index
+    if (i >= moviesarr.length){
+        res.send('no moive at index')
+    }else{
+        res.send(moviesarr[i])
+    }
+})
 
+router.get("/films", function(req, res){
+    res.send(movies)
+})
 
+router.get("/films/:filmId", function(req, res){
+    let id = parseInt(req.params.filmId)
+    console.log
+    for (let i of movies){
+        if (id === i.id){
+            res.send(i)
+        }
+    }
+    res.send("invalid id")
+})
 
 module.exports = router;
