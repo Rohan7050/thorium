665215856f62355371a286759d2b0f1d4bfc41ba
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-02-23 23:16:03 +0530

    2nd is not complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,23 +70,160 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
-const player = []
+const player =
+[ 
+    {
+
+  "name": "manish",
+
+  "dob": "1/1/1995",
+
+  "gender": "male",
+
+  "city": "jalandhar",
+
+  "sports": ["swimming"],
+
+  "bookings": 
+        [
+            {
+
+                "bookingNumber": 1,
+                "sportId"  : "cricket1234x1",
+                "centerId" : "3333",
+                "type": "private",
+                "slot": "16286598000000",
+                "bookedOn": '31/08/2021',
+                "bookedFor": '01/09/2021',
+            },
+            {
+                "bookingNumber": 2,
+                "sportId": "swiming1234x1",
+                "centerId" : "344334",
+                "type": "private",
+                "slot": '16286518000000',
+                "bookedOn": '31/08/2001',
+                "bookedFor": '01/09/2001'
+            },
+        ]
+
+    },
+
+
+
+    {
+
+  "name": "gopal",
+
+  "dob": "1/23/1995",
+
+  "gender": "male",
+
+  "city": "pune",
+
+  "sports": ["swimming","footbal"],
+
+  "bookings": 
+        [
+            {
+
+                "bookingNumber": 1,
+                "sportId"  : "cricket1234x1",
+                "centerId" : "3333",
+                "type": "private",
+                "slot": "16286598000000",
+                "bookedOn": '31/08/2021',
+                "bookedFor": '01/09/2021',
+            }
+        ]
+    }
+
+]
+
+// const player = []
 router.post("/players", function(req, res){
     const playerInfo = {}
-    let name = req.body.name
+    let name = req.body.name.toLowerCase()
     let dob = req.body.dob
     let gender = req.body.gender
     let city = req.body.city
     let sports = req.body.sports
-    let bookings = []
+    const bookings = []
     playerInfo.name = name
     playerInfo.dob = dob
     playerInfo.gender = gender
     playerInfo.city = city
     playerInfo.sports = sports
     playerInfo.booking = bookings
-    player.push(playerInfo)
+    let flag = 0
+    for (let i = 0; i < player.length; i++){
+        if (player[i].name == name){
+            res.send({player: name, exist: true, solution: "Enter different name"})
+            flag = 1
+        }
+    }
+    if (flag === 0){
+        player.push(playerInfo)
+    }
     res.send(player)
 })
 
+// {
+//     "bookingNumber": 1,s
+//     "sportId": "",
+//     "centerId": "",
+//     "type": "private",
+//     "slot": "1362379137913",
+//     "bookedOn": "1/3/2022",
+//     "bookedFor": "4/5/2022"
+// }
+
+// {
+//     "name": "Jayesh",
+//     "dob": "23/2/1999",
+//     "gender": "male", 
+//     "city": "Mumbai",  
+//     "sports": [  
+//       "football"
+//     ]
+// }
+
+router.get("/allPlayer", function(req, res){
+    res.send(player)
+})
+
+router.post("/players/:playerName/bookings/:bookingId", function (req, res){
+    let playerName = req.params.playerName.toLocaleLowerCase()
+    let bookingId = req.params.bookingId
+    let bookingNumber = req.body.bookingNumber
+    let sportId = req.body.sportId
+    let centerId = req.body.centerId
+    let type = req.body.type
+    let slot = req.body.slot
+    let bookedOn = req.body.bookedOn
+    let bookedFor = req.body.bookedFor
+    const bookingObject = {}
+    bookingObject.bookingNumber = bookingNumber
+    bookingObject.sportId = sportId
+    bookingObject.centerId = centerId
+    bookingObject.type = type
+    bookingObject.slot = slot
+    bookingObject.bookedOn = bookedOn
+    bookingObject.bookedFor = bookedFor
+    for (let i = 0; i < player.length; i++){
+        if (playerName === player[i].name){
+            // for (let i = 0; i < player.booking.length; i++){
+            //     if (player.booking[i].bookingNumber == bookingId){
+            //         res.send({ID: bookingId, exist: true})
+            //     }
+            // }
+            
+            player.booking.push(bookingObject)
+            res.send(player)
+        }
+    }
+    res.send({name: playerName, id: bookingId})
+})
+
 module.exports = router;
+// player.booking[i].bookingNumber
