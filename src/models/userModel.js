d5bb076304046cfe9c864421bf120575a5b7f005
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-02-25 19:17:09 +0530

    add comment

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -29,3 +29,18 @@ module.exports = mongoose.model('User', userSchema) //users
 
 // String, Number
 // Boolean, Object/json, array
+// {
+//     "firstName": String,
+//     "lastName": String,
+//     "mobile": {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     "emailId": String,
+//     "gender": {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     "age": Number
+// }
