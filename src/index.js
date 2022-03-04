c78bcf646c77e87860070139c128852fc30d9a50
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-03-04 20:01:25 +0530

    assignment complete (getting log after api hit)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const moment = require("moment")
+const requestIp = require("request-ip")
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
-const app = express();
 
+const app = express();
+// const dt = moment();
+
+app.use(requestIp.mw({ attributeName : 'customIp' }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next){
+    let date = moment().format()
+    // let ip = req.customIp;
+    // let routes = req.route.path
+    console.log(date, req.ip, req.originalUrl)
+    next()
+})
 
 mongoose.connect("mongodb+srv://rohan7599:MipvNOjb97usB2oZ@cluster0.lviwx.mongodb.net/mongoDBsession3?retryWrites=true&w=majority", {
     useNewUrlParser: true
---
+++ src/test.js
@@ -0,0 +1,3 @@
+const moment = require("moment")
+const dt = moment()
+console.log(dt.format('MMMM Do YYYY, h:mm:ss a'))
