ac11a735f3c26145d6698dcf8f1235ed18d0ca70
Rohan_Pagare <rohanpagare7599@gmail.com>
2022-03-11 15:55:55 +0530

    install axios

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const cowinController= require("../controllers/cowinController")
 
 
 
@@ -9,11 +9,11 @@ router.get("/test-me", function (req, res) {
 })
 
 
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
+router.get("/cowin/states", cowinController.getStates)
+router.get("/cowin/districtsInState/:stateId", cowinController.getDistricts)
+router.get("/cowin/getByPin", cowinController.getByPin)
 
-router.post("/cowin/getOtp", CowinController.getOtp)
+router.post("/cowin/getOtp", cowinController.getOtp)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
